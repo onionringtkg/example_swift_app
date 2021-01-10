e140785b38d7d355411f325d728302e5a5968aeb
Okano Takumi <oknokn.of.12@gmail.com>
2021-01-10 16:49:45 +0900

    Initial Commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by 岡野拓巳 on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
